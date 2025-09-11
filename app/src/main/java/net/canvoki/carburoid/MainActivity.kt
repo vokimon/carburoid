8dea0a56a04448bba8757bce98026c805d30a44d
David García Garzón <voki@canvoki.net>
2025-09-11 03:48:19 +0200

    🏗️ template application

---
+++ app/src/main/java/net/canvoki/carburoid/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.canvoki.carburoid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
