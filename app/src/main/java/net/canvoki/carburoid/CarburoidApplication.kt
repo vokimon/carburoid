d25a41e5e7e3a7fc7b12ea0a4d9f60d6148b8e1d
David García Garzón <voki@canvoki.net>
2025-09-15 23:54:35 +0200

    ⚡️ On debug use a preloaded json for quick testing

--- app/src/main/java/net/canvoki/carburoid/CarburoidApplication.kt
+++ app/src/main/java/net/canvoki/carburoid/CarburoidApplication.kt
@@ -3,6 +3,7 @@ package net.canvoki.carburoid
 import android.app.Application
 import android.content.Context
 import java.io.File
+import java.io.FileNotFoundException
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,16 +16,30 @@ class CarburoidApplication : Application() {
 
     private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
+    lateinit private var cacheFile: File
     lateinit var repository: GasStationRepository
         private set
 
     override fun onCreate() {
         super.onCreate()
+        cacheFile = File(filesDir, "gas_stations_cache.json")
+        setupDebugData()
         repository = setupRepository()
     }
 
+    fun setupDebugData() {
+        if (cacheFile.exists()) return
+        val mockJson = try {
+            assets.open("stations-debug.json").bufferedReader().use { it.readText() }
+        } catch (e: FileNotFoundException) {
+            log("MOCK DATA NOT FOUND. RELEASE?")
+            return
+        }
+        log("USING MOCK DATA. DEBUG?")
+        cacheFile.writeText(mockJson)
+    }
+
     fun setupRepository() : GasStationRepository {
-        val cacheFile = File(filesDir, "gas_stations_cache.json")
         val api = GasStationApiFactory.create()
         val repository = GasStationRepository(
             api = api,
