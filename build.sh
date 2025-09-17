5cf9526a339a5f3dfd760bddea2363b97b657a0c
David García Garzón <voki@canvoki.net>
2025-09-17 11:17:36 +0200

    🏗️ build script commited
    Non standard but practical for me

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export ANDROID_HOME="/usr/lib/android-sdk"
+
+run() {
+    echo -e "\033[34m:: $@\033[0m"
+    "$@"
+}
+
+run ./gradlew assembleDebug &&
+    run ./gradlew testDebugUnitTest --info &&
+    (run adb uninstall net.canvoki.carburoid || true ) &&
+    run ./gradlew installDebug &&
+    run adb shell am start -n net.canvoki.carburoid/.MainActivity &&
+    echo done
+
+
+# adb shell cmd uimode night no
+# adb shell cmd uimode night yes
+
