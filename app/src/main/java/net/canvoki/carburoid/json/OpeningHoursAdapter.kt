4648019911cb584492b2eba788f9fafcf388b8c4
David García Garzón <voki@canvoki.net>
2025-09-20 23:06:53 +0200

    ✅ OpeningHoursAdapter

---
+++ app/src/main/java/net/canvoki/carburoid/json/OpeningHoursAdapter.kt
@@ -0,0 +1,22 @@
+package net.canvoki.carburoid.json
+
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonWriter
+import com.google.gson.stream.JsonReader
+import com.google.gson.JsonParseException
+import com.google.gson.stream.JsonToken
+import net.canvoki.carburoid.model.OpeningHours
+
+class OpeningHoursAdapter : TypeAdapter<OpeningHours>() {
+    override fun write(out: JsonWriter, value: OpeningHours?) {
+        out.value(value?.toString()?: "")
+    }
+
+    override fun read(`in`: JsonReader): OpeningHours? {
+        if (`in`.peek() == JsonToken.NULL) {
+            `in`.nextNull()
+            return null
+        }
+        return OpeningHours.parse(`in`.nextString())
+    }
+}
