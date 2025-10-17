2551f374c889b755e6f22a1bea8d15402836947b
David García Garzón <voki@canvoki.net>
2025-10-17 04:28:58 +0200

    🏗️ fdroid: use provisioner

--- tools/fdroid-build.sh
+++ tools/fdroid-build.sh
@@ -12,14 +12,15 @@ export serverwebroot=$(pwd)/build
 run echo $serverwebroot
 export PATH="$fdroidserver:$PATH" PYTHONPATH="$fdroidserver"
 export JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' | awk -F'=' '{print $2}' | tr -d ' ')
-export
-#run sdkmanager --install "platform-tools" "build-tools;34.0.0" "platforms;android-34" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;23.2.8568313"
+run sudo bash /home/vagrant/fdroidserver/buildserver/provision-gradle
+run sdkmanager --install "platform-tools" "build-tools;36.0.0" "platforms;android-36" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;23.2.8568313"
 #run sudo apt-get update -y
 #run sudo apt-get install -y pkgconf clang scons imagemagick python3-pip python-is-python3 apksigner
 cd /build
-run fdroid readmeta
-run fdroid rewritemeta $APP_ID
-run fdroid checkupdates --allow-dirty $APP_ID
-run fdroid lint $APP_ID
+run fdroid readmeta -v
+run fdroid rewritemeta -v $APP_ID
+run fdroid lint -v $APP_ID
+run fdroid checkupdates -v --auto --allow-dirty $APP_ID
+run fdroid update -v --create-metadata net.canvoki.carburoid
 run fdroid build -vl $APP_ID
 
