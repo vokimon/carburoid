f2637576d54d6dc7d592eb3e11c8a906c3b46fa9
David García Garzón <voki@canvoki.net>
2025-10-19 10:45:40 +0200

    🧹 clean warn Optin experimental Coroutines Api

--- app/src/test/java/net/canvoki/carburoid/test/TestHelpers.kt
+++ app/src/test/java/net/canvoki/carburoid/test/TestHelpers.kt
@@ -3,6 +3,8 @@ package net.canvoki.carburoid.test
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runCurrent
 
+@OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
+
 /**
  * Drains all immediately runnable coroutines in the test dispatcher.
  * Safe, stable, non-experimental replacement for advanceUntilIdle().
