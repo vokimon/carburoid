c0a99c6adac9d0bc609fbc3b4c5898163fbedc4f
David García Garzón <voki@canvoki.net>
2025-11-28 16:27:51 +0100

    🐛 missing import

--- app/src/main/java/net/canvoki/carburoid/plotnavigator/GasStationCard.kt
+++ app/src/main/java/net/canvoki/carburoid/plotnavigator/GasStationCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
