0f4fd0e1069494cef9ba743fa1653e99f87cfe4a
David García Garzón <voki@canvoki.net>
2025-12-01 14:28:19 +0100

    🎨 Applied style to ProductCatalog

--- app/src/main/java/net/canvoki/carburoid/product/ProductCatalog.kt
+++ app/src/main/java/net/canvoki/carburoid/product/ProductCatalog.kt
@@ -6,9 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import net.canvoki.carburoid.R
 
-
 object ProductCatalog {
-
     data class Product(
         val apiName: String,
         @field:StringRes val name: Int? = null,
@@ -23,66 +21,76 @@ object ProductCatalog {
         listOf(
             ProductCategory(
                 name = R.string.product_category_diesel,
-                products = listOf(
-                    Product("Gasoleo A", R.string.product_name_gasoleo_a),
-                    Product("Gasoleo Premium"),
-                    Product("Gasoleo B"),
-                    Product("Diésel Renovable"),
-                ),
+                products =
+                    listOf(
+                        Product("Gasoleo A", R.string.product_name_gasoleo_a),
+                        Product("Gasoleo Premium"),
+                        Product("Gasoleo B"),
+                        Product("Diésel Renovable"),
+                    ),
             ),
             ProductCategory(
                 name = R.string.product_category_gasoline,
-                products = listOf(
-                    Product("Gasolina 95 E10"),
-                    Product("Gasolina 95 E25"),
-                    Product("Gasolina 95 E5"),
-                    Product("Gasolina 95 E5 Premium"),
-                    Product("Gasolina 95 E85"),
-                    Product("Gasolina 98 E10"),
-                    Product("Gasolina 98 E5"),
-                    Product("Gasolina Renovable"),
-                ),
+                products =
+                    listOf(
+                        Product("Gasolina 95 E10"),
+                        Product("Gasolina 95 E25"),
+                        Product("Gasolina 95 E5"),
+                        Product("Gasolina 95 E5 Premium"),
+                        Product("Gasolina 95 E85"),
+                        Product("Gasolina 98 E10"),
+                        Product("Gasolina 98 E5"),
+                        Product("Gasolina Renovable"),
+                    ),
             ),
             ProductCategory(
                 name = R.string.product_category_natural_gas,
-                products = listOf(
-                    Product("Gas Natural Comprimido"),
-                    Product("Gas Natural Licuado"),
-                    Product("Biogas Natural Comprimido"),
-                    Product("Biogas Natural Licuado"),
-                ),
+                products =
+                    listOf(
+                        Product("Gas Natural Comprimido"),
+                        Product("Gas Natural Licuado"),
+                        Product("Biogas Natural Comprimido"),
+                        Product("Biogas Natural Licuado"),
+                    ),
             ),
             ProductCategory(
                 name = R.string.product_category_biofuels,
-                products = listOf(
-                    Product("Biodiesel"),
-                    Product("Bioetanol"),
-                ),
+                products =
+                    listOf(
+                        Product("Biodiesel"),
+                        Product("Bioetanol"),
+                    ),
             ),
             ProductCategory(
                 name = R.string.product_category_other_gases,
-                products = listOf(
-                    Product("Gases licuados del petróleo"),
-                    Product("Hidrogeno"),
-                    Product("Amoniaco"),
-                    Product("Metanol"),
-                ),
+                products =
+                    listOf(
+                        Product("Gases licuados del petróleo"),
+                        Product("Hidrogeno"),
+                        Product("Amoniaco"),
+                        Product("Metanol"),
+                    ),
             ),
             ProductCategory(
                 name = R.string.product_category_additives,
-                products = listOf(
-                    Product("Adblue"),
-                ),
+                products =
+                    listOf(
+                        Product("Adblue"),
+                    ),
             ),
         )
     }
 
     private val productMap: Map<String, Product> by lazy {
-        categories.flatMap { it.products }
+        categories
+            .flatMap { it.products }
             .associateBy { it.apiName }
     }
 
-    fun productName(apiName: String, context: Context): String {
+    fun productName(
+        apiName: String,
+        context: Context,
+    ): String {
         val product = productMap[apiName]
         return product?.name?.let { context.getString(it) } ?: apiName
     }
@@ -95,10 +103,9 @@ object ProductCatalog {
 }
 
 @Composable
-fun translateProductName(apiName: String): String {
-    return ProductCatalog.productName(apiName)
-}
+fun translateProductName(apiName: String): String = ProductCatalog.productName(apiName)
 
-fun translateProductName(apiName: String, context: Context): String {
-    return ProductCatalog.productName(apiName, context)
-}
+fun translateProductName(
+    apiName: String,
+    context: Context,
+): String = ProductCatalog.productName(apiName, context)
