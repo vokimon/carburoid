6dc7ec3d99353c1bc47be40bdfda7242260d477e
David García Garzón <voki@canvoki.net>
2025-12-01 23:07:11 +0100

    ♻️ ViewModel: PlotNavigatorActivity uses it

--- app/src/main/java/net/canvoki/carburoid/plotnavigator/PlotNavigatorActivity.kt
+++ app/src/main/java/net/canvoki/carburoid/plotnavigator/PlotNavigatorActivity.kt
@@ -3,17 +3,18 @@ package net.canvoki.carburoid.plotnavigator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.core.view.WindowCompat
 import net.canvoki.carburoid.CarburoidApplication
-import net.canvoki.carburoid.repository.GasStationRepository
-import net.canvoki.carburoid.ui.settings.ThemeSettings
+import net.canvoki.carburoid.model.GasStation
+import net.canvoki.carburoid.MainSharedViewModel
 
 class PlotNavigatorActivity : ComponentActivity() {
-    private val app: CarburoidApplication
-        get() = application as CarburoidApplication
 
-    private val repository: GasStationRepository
-        get() = app.repository
+    private val viewModel: MainSharedViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +22,10 @@ class PlotNavigatorActivity : ComponentActivity() {
         // Required in Android 16+
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        val stations = repository.getData()?.stations ?: emptyList()
-
         setContent {
+            val stations by viewModel.stationsUpdated.collectAsState(
+                initial = viewModel.getStationsToDisplay()
+            )
             PortNavigatorScreen(stations)
         }
     }
