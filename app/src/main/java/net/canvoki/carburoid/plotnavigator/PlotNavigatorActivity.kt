b667d35e0a3eb56dc7d10a7c8cce79fecb7637d6
David García Garzón <voki@canvoki.net>
2025-12-01 22:11:55 +0100

    ♻️ PlotNavigatorActivity: simplified Composable load

--- app/src/main/java/net/canvoki/carburoid/plotnavigator/PlotNavigatorActivity.kt
+++ app/src/main/java/net/canvoki/carburoid/plotnavigator/PlotNavigatorActivity.kt
@@ -2,11 +2,9 @@ package net.canvoki.carburoid.plotnavigator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.platform.ComposeView
+import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import net.canvoki.carburoid.CarburoidApplication
-import net.canvoki.carburoid.R
-import net.canvoki.carburoid.model.GasStation
 import net.canvoki.carburoid.repository.GasStationRepository
 import net.canvoki.carburoid.ui.settings.ThemeSettings
 
@@ -23,12 +21,9 @@ class PlotNavigatorActivity : ComponentActivity() {
         // Required in Android 16+
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        setContentView(R.layout.activity_chart_view)
-
-        val composeView = findViewById<ComposeView>(R.id.chartView)
         val stations = repository.getData()?.stations ?: emptyList()
 
-        composeView.setContent {
+        setContent {
             PortNavigatorScreen(stations)
         }
     }
