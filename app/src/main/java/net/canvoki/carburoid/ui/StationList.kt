75135014707841bdfb55c5bbf316f4802f13b3e1
David García Garzón <voki@canvoki.net>
2025-12-23 12:59:38 +0100

    💄 Background icon on the station list when empty

--- app/src/main/java/net/canvoki/carburoid/ui/StationList.kt
+++ app/src/main/java/net/canvoki/carburoid/ui/StationList.kt
@@ -6,13 +6,14 @@ import android.widget.FrameLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,10 +24,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -59,11 +62,19 @@ fun PullOnRefresh(
 
 @Composable
 fun NoStationsPlaceholder(modifier: Modifier = Modifier) {
-    Column(
+    Box(
         modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        contentAlignment = Alignment.Center,
     ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_local_gas_station),
+            contentDescription = null,
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .alpha(0.1f),
+            tint = MaterialTheme.colorScheme.onSurface,
+        )
         Text(
             text = stringResource(R.string.no_gas_stations),
             color = MaterialTheme.colorScheme.onSurfaceVariant,
