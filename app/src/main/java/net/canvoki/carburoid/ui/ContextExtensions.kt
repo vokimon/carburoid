b22ce264cdc594c11790e5334476134785bbedcd
David García Garzón <voki@canvoki.net>
2025-12-24 01:32:42 +0100

    🐛 openActivity: explicit template preferred

--- app/src/main/java/net/canvoki/carburoid/ui/ContextExtensions.kt
+++ app/src/main/java/net/canvoki/carburoid/ui/ContextExtensions.kt
@@ -4,11 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.ComponentActivity
 
-fun Context.openActivity(
-    activityClass: Class<out ComponentActivity>,
-    configure: Intent.() -> Unit = {},
+inline fun <reified T : ComponentActivity> Context.openActivity(
+    noinline configure: Intent.() -> Unit = {}
 ): Boolean {
-    val intent = Intent(this, activityClass)
+    val intent = Intent(this, T::class.java)
     intent.configure()
     startActivity(intent)
     return true
