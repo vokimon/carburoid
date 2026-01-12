a7204fae170bdcdfaea255b0a0cd6cfaf20aa909
David García Garzón <voki@canvoki.net>
2026-01-12 18:55:15 +0100

    🎨 import order

--- app/src/main/java/net/canvoki/carburoid/MainActivity.kt
+++ app/src/main/java/net/canvoki/carburoid/MainActivity.kt
@@ -20,8 +20,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
