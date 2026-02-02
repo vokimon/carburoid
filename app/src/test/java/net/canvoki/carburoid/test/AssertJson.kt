a2950365408554ad05a290f8492c3174c65d0107
David García Garzón <voki@canvoki.net>
2026-02-02 14:14:11 +0100

    ✅ ensure test is normalized, but show pretty

--- app/src/test/java/net/canvoki/carburoid/test/AssertJson.kt
+++ app/src/test/java/net/canvoki/carburoid/test/AssertJson.kt
@@ -1,10 +1,29 @@
 package net.canvoki.carburoid.test
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 import org.junit.Assert.assertEquals
 
-fun canonicalizeJson(json: String): String {
-    val result = Json.parseToJsonElement(json).toString()
+private fun JsonElement.canonicalize(): JsonElement =
+    when (this) {
+        is JsonObject -> JsonObject(
+            entries
+                .sortedBy { it.key }
+                .associate { it.key to it.value.canonicalize() }
+        )
+        is JsonArray -> JsonArray(map { it.canonicalize() })
+        else -> this
+    }
+
+private val json = Json {
+    prettyPrint = true
+    prettyPrintIndent = "  "
+}
+
+private fun canonicalizeJson(jsonStr: String): String {
+    val result = json.parseToJsonElement(jsonStr).canonicalize().toString()
     return result
 }
 
