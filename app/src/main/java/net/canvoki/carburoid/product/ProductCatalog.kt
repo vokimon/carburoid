0e5d44c001145f245e3fee45ee1f1ae6f29a7ed5
David García Garzón <voki@canvoki.net>
2026-02-02 23:35:52 +0100

    ✨ France product catalog
    Most products translations mapped to spanish ones (hope they match)

--- app/src/main/java/net/canvoki/carburoid/product/ProductCatalog.kt
+++ app/src/main/java/net/canvoki/carburoid/product/ProductCatalog.kt
@@ -105,6 +105,36 @@ object SpainProductCatalog : ProductCatalogProvider() {
     }
 }
 
+object FranceProductCatalog : ProductCatalogProvider() {
+    override val categories: List<ProductCategory> by lazy {
+        listOf(
+            ProductCategory(
+                name = R.string.product_category_diesel,
+                products =
+                    listOf(
+                        Product("gazole", R.string.product_name_gasoleo_a),
+                    ),
+            ),
+            ProductCategory(
+                name = R.string.product_category_gasoline,
+                products =
+                    listOf(
+                        Product("sp95", R.string.product_name_gasolina_95_e5),
+                        Product("sp98", R.string.product_name_gasolina_98_e5),
+                        Product("e10", R.string.product_name_gasolina_95_e10),
+                        Product("e85", R.string.product_name_gasolina_95_e85),
+                    ),
+            ),
+            ProductCategory(
+                name = R.string.product_category_gaseous,
+                products =
+                    listOf(
+                        Product("gplc", R.string.product_name_gases_licuados_del_petroleo),
+                    ),
+            ),
+        )
+    }
+}
 
 @Composable
 fun translateProductName(apiName: String): String = ProductCatalog.productName(apiName)
