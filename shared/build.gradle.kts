7aa4a90c384c2c0729fedebfb9e50f2a14946fcf
David García Garzón <voki@canvoki.net>
2026-02-14 11:24:03 +0100

    🏗️ added an empty shared library

---
+++ shared/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+//    alias(libs.plugins.android.library)
+//    alias(libs.plugins.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "net.canvoki.shared"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
+
+dependencies {
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.compose.runtime)
+}
